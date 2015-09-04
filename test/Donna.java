c76e8dcb9010dc5912278e8caa1dff4cbbecb35e
xuanshenbo <xuanshenbo@hotmail.com>
2015-09-04 14:12:35 +1200

    first commit

---
+++ test/Donna.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Donna {
+	public static void main(String[] args) {
+		System.out.println("Donna");
+	}
+}
---
+++ test/Gareth.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Gareth {
+	public static void main(String[] args) {
+		System.out.println("Gareth");
+	}
+}
